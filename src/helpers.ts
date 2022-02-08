e469de03ebf95ed78c9460dcef8c161be7c2cc5a
Guilherme Eduardo <guiedugt@gmail.com>
2022-02-08 17:31:31 -0500

    Add # to rgbToHex output

--- src/helpers.ts
+++ src/helpers.ts
@@ -24,7 +24,7 @@ export const rgbToHex = (rgb: IRGB): string => {
     return char;
   };
 
-  return `${getHex(r)}${getHex(g)}${getHex(b)}`;
+  return `#${getHex(r)}${getHex(g)}${getHex(b)}`;
 };
 
 export const getRGBListAvg = (rgbList: IRGB[]): IRGB => {
